bf1b2a165f5792abc32cd776bf1604360fa76c55
Chase Barman <chasedbarman@gmail.com>
2024-06-25 08:01:09 -0700

    Draw to screen

--- src/Game.cpp
+++ src/Game.cpp
@@ -28,12 +28,13 @@ Game::Game() {
     std::cerr << "SDL_Mixer has failed: " << Mix_GetError() << std::endl;
   }
 
-  // Create window and renderer
+  // Window
   this->window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, SDL_WINDOW_SHOWN);
   if (this->window == NULL) {
     std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
   }
 
+  // Renderer
   this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (this->renderer == NULL) {
     std::cerr << "Failed to render window: " << SDL_GetError() << std::endl;
@@ -51,7 +52,26 @@ void Game::loop() {
       if (event.type == SDL_QUIT) {
         is_running = false;
       }
+      else if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+          case SDLK_RETURN:
+            is_running = false;
+            break;
+          case SDLK_w:
+            break;
+          case SDLK_s:
+            break;
+          case SDLK_a:
+            break;
+          case SDLK_d:
+            break;
+        }
+      }
     }
+
+    this->clearScreen();
+    this->draw();
+    SDL_RenderPresent(this->renderer);
 
     // Cap framerate
     SDL_Delay(std::floor(FRAME_INTERVAL - getElapsedTime(start_time)));
@@ -60,4 +80,35 @@ void Game::loop() {
 
 float Game::getElapsedTime(uint32_t last_time) {
   return SDL_GetTicks() - last_time;
+}
+
+void Game::clearScreen() {
+  setDrawColor(&COLOR_BG);
+  SDL_RenderClear(this->renderer);
+}
+
+void Game::setDrawColor(SDL_Color *color) {
+  SDL_SetRenderDrawColor(this->renderer, color->r, color->g, color->b, color->a);
+}
+
+void Game::draw() {
+  SDL_Rect rect;
+  rect.x = 100;
+  rect.y = 100;
+  rect.w = 100;
+  rect.h = 100;
+
+  this->setDrawColor(&COLOR_RED);
+  SDL_RenderFillRect(this->renderer, &rect);
+}
+
+Game::~Game() {
+  TTF_CloseFont(this->font);
+  TTF_Quit();
+
+  SDL_DestroyRenderer(this->renderer);
+  SDL_DestroyWindow(this->window);
+
+  SDL_Quit();
+  std::cout << "Successfully cleaned up game objects!" << std::endl;
 }
--- src/Game.hpp
+++ src/Game.hpp
@@ -6,6 +6,17 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 
+static SDL_Color COLOR_BG = { 0, 0, 0, 255 };
+static SDL_Color COLOR_BLACK = { 0, 0, 0, 255 };
+static SDL_Color COLOR_WHITE = { 255, 255, 255, 255 };
+static SDL_Color COLOR_LIGHT_BLUE = { 173, 216, 230, 255 };
+static SDL_Color COLOR_DARK_BLUE = { 0, 0, 139, 255 };
+static SDL_Color COLOR_ORANGE = { 255, 165, 0, 255 };
+static SDL_Color COLOR_YELLOW = { 255, 255, 0, 255 };
+static SDL_Color COLOR_GREEN = { 0, 128, 0, 255 };
+static SDL_Color COLOR_PURPLE = { 128, 0, 128, 255 };
+static SDL_Color COLOR_RED = { 255, 0, 0, 255 };
+
 class Game {
   SDL_Window *window;
   SDL_Renderer *renderer;
@@ -13,6 +24,10 @@ class Game {
 
   public:
     Game();
+    ~Game();
     void loop();
     float getElapsedTime(uint32_t last_time);
+    void clearScreen();
+    void setDrawColor(SDL_Color *color);
+    void draw();
 };
